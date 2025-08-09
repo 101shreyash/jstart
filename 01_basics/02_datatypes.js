3416126c2db9d9c34c87e1ef94bf7a0ba6556dd8
Shreyash <himeshreyash@gmail.com>
2025-08-09 05:06:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict";
+/* 
+Enables strict mode to catch common mistakes and prevent unsafe actions.
+Use it at the top of your file to apply strict mode to the entire script.
+Also makes JavaScript behave like a newer, safer version.
+*/
+
+
+// alert(3+3) // we are using node js not browser 
+
+console.log( 3
++
+3
+)  // code readability should be high
+
+console.log(3+3)
+
+
+
+let name ="shreyash" //str ""
+let age = "17"   // number => 2 to power 53
+let isloggedIn = false  // Boolean true/false
+let State; //Undefined
+// BigInt //9007199254740991n // If you want super big integers beyond 
+// null  => Stand Alone value
+//undefined =>
+//symbol => unique
+
+//object =>
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  // object 
+
+
+
+
+
