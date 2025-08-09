1cc43eeb21462a263756d2d3df6d4afd381d672d
Shreyash <himeshreyash@gmail.com>
2025-08-09 05:05:43 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 414141
+let accountEmail = "getonthebarsbro"
+var accountPassword = "12345"
+accountCity="Biratnagar"
+
+
+
+// accountId = 515151 // not allowed
+
+/*
+Avoid var because it can cause scope problems. Use let or const instead.
+
+ */
+AccountEmail = "getonthefloorsbro"
+accountPassword = "112345"
+accountCity = "jhapa"
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
+
+
+
+
