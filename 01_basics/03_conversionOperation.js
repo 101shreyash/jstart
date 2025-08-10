617d55227613a35ce744ac6c8e946f438b052702
Shreyash <himeshreyash@gmail.com>
2025-08-10 13:06:36 +0000

    ConversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,7 +37,7 @@ console.log( typeof stringNumber);
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+console.log(negValue); //-3
 
 
 // console.log(2+2);
@@ -51,20 +51,29 @@ let str1 = "hello"
 let str2 = " shreyash"
 
 let sumof2str = str1+str2
-console.log(sumof2str);
+console.log(sumof2str); // "hello shreyash"
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+console.log("1" + 2);  // => 12
+console.log(1 + "2");  //=> 12
+console.log("1" + 2 + 2); //=> 122
+console.log(1 + 2 + "2"); //=> 32
 
 
+let num1,  num2,  num3 
+num1 = num2 = num3 = 2+2
+console.log(num1 , num2 , num3); // not a production level code ,
+//  readability should be high ,  but the output would be 4 4 4 
 
+ let gameCounter = 100
+ gameCounter++
+ console.log(gameCounter); //101 even when done ++gameCounter 
+ 
 
 
  
 
-
+// Learn prefix (++i) and postfix (i++) operators from MDN
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 
 
 
