b2932836b0d1ec089cc6c326300a8784d84d4d5d
Shreyash <himeshreyash@gmail.com>
2025-08-10 09:22:46 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,31 @@
-let score = 97
+let score = "33"
 
 console.log(typeof score);
+
+let valueInNumber= Number(score)
+console.log( typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 
+// false => 0
+
+
+let isloggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true 
+// 0 => false
+// "shreyash" => true
+// "" => False
+
+
+let someNumber = 56
+
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber);
