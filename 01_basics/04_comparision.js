ba54acbe1a2a9e6e4fa3fe7f8cf8e7881a65cf11
Shreyash <himeshreyash@gmail.com>
2025-08-11 12:47:17 +0000

    Comparisions

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -20,3 +20,25 @@ console.log(null >= 0);
 
 
 
+console.log(undefined == 0); //false
+console.log(undefined > 0 ); //fasle
+console.log(undefined < 0 ); //false
+
+// ===
+
+console.log("2" === 2); // false
+// === → Strict equality
+// Compares value AND type.
+// "2" is a string, 2 is a number → types don’t match → returns false.
+
+
+console.log("2" == 2); 
+// == → Loose equality
+// If types differ, JS will coerce one to match the other before comparing.
+// "2" (string) gets converted to 2 (number) → now it’s 2 == 2 → returns true.
+
+
+
+
+
+
