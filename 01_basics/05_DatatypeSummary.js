f223560491030f886f7ea1d58608d976faf1953a
Shreyash <himeshreyash@gmail.com>
2025-08-12 09:54:02 +0000

    SummaryofDatatypes

---
+++ 01_basics/05_DatatypeSummary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 types 
+// String , Number , Boolean , null , undefined , BigInt
+
+
+// JavaScript is dynamically typed, not statically typed.
+// In dynamically typed languages, type checking happens at runtime, not before running the code.
+// You don’t have to declare what type a variable will be JS figures it out while running.
+// You can even change a variable’s type on the fly without errors. for eg:
+let x = 10;     // number
+x = "hello";    // now a string no complaint from JS
+
+
+// In a statically typed language like C, Java, or TypeScript:
+// int x = 10;  
+// x = "hello"; //  Compile-time error
+
+
+// #Reference (Non primitive)
+// Array , Object Function
+
+
+const Superheros = ["Batman" , "Ironman", "BlackPanther"]
+
+let myObj = {
+    name:"shreyash",
+    age:17,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+// Data types of Nonprimitive datatypes are always obj except for the function whose datatype is fucntionobject
+
