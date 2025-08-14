6f7f3913cca470e5e9421eea6078e8486b6d346b
Shreyash <himeshreyash@gmail.com>
2025-08-14 13:18:55 +0000

    Maths & Num

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,16 @@
+const score = 455
+console.log(score);
+
+const grade =  new Number(455)
+console.log(grade);
+console.log(grade.toString().length);
+console.log(grade.toFixed(5)); // 455.00000 ,  Rounds number to fixed decimals and returns it as a string
+
+
+const otherNumber = 1234.50
+console.log(otherNumber.toPrecision(4));
+
+
+
+
+
