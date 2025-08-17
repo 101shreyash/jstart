a86a7a243d52aae2cf56baa02c8ce203fbe81375
Shreyash <himeshreyash@gmail.com>
2025-08-17 08:54:39 +0000

    Date And Time

--- 01_basics/08_date_and_time.js
+++ 01_basics/08_date_and_time.js
@@ -1,3 +1,8 @@
+
+// Date : Date is an object in javascript. 
+// JavaScript stores dates as number of milliseconds since January 01, 1970
+
+
 const myDate = new Date()
 //creates a Date object holding the current date and time isn’t very humanreadable.
 console.log(myDate); 
@@ -25,12 +30,41 @@ console.log(myCreatedDAte.toDateString());
 // In JS Date: 0 = January, 1 = February, ... , 11 = December
 
 
+
 const dateAndTime = new Date(2025 , 0 , 11  , 5 , 3, 2, 1 )
 console.log(dateAndTime.toLocaleString());
 
 
 let anotherDate = new Date("2025-09-3") // YYYY-MM-DD
 console.log(anotherDate);
+
+
+let myTimeStamps = Date.now()
+
+console.log(myTimeStamps); 
+//prints the timestamp (ms since Jan 1, 1970) for myTimeStamps
+
+console.log(anotherDate.getTime()); 
+// prints the timestamp (ms since Jan 1, 1970) for anotherDate
+
+console.log(Math.floor(Date.now()/1000));
+// Print current Unix timestamp in seconds (time since Jan 1, 1970 UTC)
+
+console.log(Math.floor(Date.now() / (1000 * 60 * 60 * 24)));
+// Print total number of days passed since Jan 1, 1970 UTC (Unix epoch)
+
+
+const recentDate = new Date()
+console.log(recentDate.getDay()); 
+// Returns the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+
+console.log(recentDate.getFullYear()); 
+
+// Returns the full year (4-digit number, e.g., 2025)
+
+console.log(recentDate.getMonth()); 
+// Returns the month number (0 = January, 1 = February, ..., 11 = December)
+
 
 
 
