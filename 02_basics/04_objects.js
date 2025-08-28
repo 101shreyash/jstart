32cf1f1397e5423e80975b401e4f44cb977c1afb
Shreyash <himeshreyash@gmail.com>
2025-08-28 12:03:51 +0000

    Objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -12,7 +12,7 @@
 
 const tinderUser = new Object() 
 
-tinderUser.name = "Omshrestha"
+tinderUser.name = "Omshresth"
 tinderUser.age = 17
 tinderUser.bio = "if she can say a b c she is redy for the d "
 tinderUser.height = "5foot9"
@@ -87,10 +87,57 @@ const regularUser = {
 //   Preferred in production (React/Next/etc.)
 //   Shallow copy only (nested objects share reference)
 
+// **********************************************************
+
+// this is an array of objects 
+// Each {} is one user’s info, and [] is the list (collection) of them.
+const users = [{
+    id:545454,
+    email: "sg@gmail.com"
+    } , 
+    {
+    id:414141,
+    email: "getonthebars@gmail.com"
+    } , 
+    
+]
 
 
 
+// console.log(users[0]); // { id: 545454, email: 'sg@gmail.com' }
+// console.log(users[1].email); // getonthebars@gmail.com
 
+
+// console.log(Object.keys(tinderUser));
+
+// [ 'name', 'age', 'bio', 'height' ]
+// Object.keys() takes the property names of the object
+// and puts them inside an array so we can loop or access them easily
+
+// console.log(Object.values(tinderUser)); 
+/* 
+['Omshresth',
+  17,
+  'if she can say a b c she is redy for the d ',
+  '5foot9'
+]   .values return the value of an object in an array form  */ 
+ 
+// console.log(Object.entries(tinderUser));
+/* 
+[
+  [ 'name', 'Omshresth' ],
+  [ 'age', 17 ],
+  [ 'bio', 'if she can say a b c she is redy for the d ' ],
+  [ 'height', '5foot9' ]
+] */  // Object.entries() gives back an array of [key,value] pairs
+
+
+console.log(tinderUser.hasOwnProperty("height")); //true 
+console.log(tinderUser.hasOwnProperty("weight")); //false 
+
+// hasOwnProperty() checks if "height" is really a property of tinderUser
+// → returns true if present, false if missing
+// ONLY checks for the key (property name), not the value.
 
 
 
