028f12530c90b854a8192a41eddbb05b8282bfe5
Shreyash <himeshreyash@gmail.com>
2025-08-30 10:56:37 +0000

    Objdestructure&JSON

--- 02_basics/05_Objectdestructure.js
+++ 02_basics/05_Objectdestructure.js
@@ -24,3 +24,23 @@ console.log(courseInstructor, courseName, coursePrice ); // Harkirat cohort3 599
 // Instead of writing course.courseInstructor, course.coursePrice, etc.
 // again and again, you can pull them out in one shot.
  
+// *********************** Javascript Object Notation (JSON) ***************
+
+//  {
+//  "name" : "Shreyash", 
+//  "twitter" : "shreyashtwts"
+//  }
+
+ // In JSON, all keys must be in double quotes " ".
+// Single quotes ' ' or no quotes at all = invalid JSON.
+// Also In JSON You can’t console.log(name) directly, it’s just text.
+// JSON = pure data in text form, not a variable, not alive in memory
+// JS Object = JSON assigned to a variable, now alive in memory and usable in code
+// Key takeaway: JSON is static text, JS object is dynamic datain-action
+
+
+[
+  {"name": "Shreyash"},
+  {"age": 17},
+  {"hobby": "liftingheavyshits"}
+]
