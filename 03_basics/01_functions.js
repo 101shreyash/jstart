072dda21505a456a5a48632e4c4f92ab94f83158
Shreyash <himeshreyash@gmail.com>
2025-09-01 11:23:50 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,1 +1,27 @@
-console.log("functions");
+// // A function is just a block of code with a name.
+// // You wrap some steps inside it, give it a name, and later you can run all those steps
+// //  just by calling that name.
+// // instead of writing the same steps again and again, you pack them inside a function.
+
+// function sayMyName (){
+// console.log("S");
+// console.log("H");
+// console.log("R");
+// console.log("E");
+// console.log("Y");
+// console.log("A");
+// console.log("S");
+// console.log("H");
+
+// }
+// // call (execute) the function
+// sayMyName()
+
+function addTwoNumbers (Number1, Number2){ //parameters
+    console.log(Number1+Number2);  
+}
+addTwoNumbers( ) // NaN // arguements
+addTwoNumbers(7, 7 ) // 14 
+addTwoNumbers(7, "7" ) // 77 (autoconversion thought the first 7 as a string )
+addTwoNumbers(7, "a" ) // 7a
+addTwoNumbers(7, null ) // 7
