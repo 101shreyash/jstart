55657beed54d85527c067965636aedaead0d0cae
Shreyash <himeshreyash@gmail.com>
2025-09-02 13:04:59 +0000

    trainingground

---
+++ 03_basics/test.js
@@ -0,0 +1,9 @@
+function myFunction (Number1  , Number2){
+ console.log(Number1*Number2);
+ 
+}
+ const Output = myFunction(4, 5) // 20 
+ console.log( " Results:", Output);  Results: undefined 
+ 
+
+ 
