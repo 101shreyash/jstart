4c36810b5627e0c03281966d0d280802badbcc9e
Shreyash <himeshreyash@gmail.com>
2025-09-03 11:59:20 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,18 +26,45 @@
 
 
 
-function  sumOfTwoNumbers (Number1  , Number2){
-    // let Output = Number1 + Number2    // wiritng this stuff has two ways one of this is this one
-    // return Output
+// function  sumOfTwoNumbers (Number1  , Number2){
+//     // let Output = Number1 + Number2    // wiritng this stuff has two ways one of this is this one
+//     // return Output
 
-    return Number1 + Number2 // &Heres the easy way to do so without even allocating the variableSpace
+//     return Number1 + Number2 // &Heres the easy way to do so without even allocating the variableSpace
 
-    console.log("Shreyash"); // Any code after return (like  console.log("Shreyash")) won’t execute.
-    //Once return runs, the function stops immediately.
+//     console.log("Shreyash"); // Any code after return (like  console.log("Shreyash")) won’t execute.
+//     //Once return runs, the function stops immediately.
+
+// }
+//  const Output = sumOfTwoNumbers(4, 5) 
+//  console.log( " Results:", Output);  //  Results: 9
+ 
+
+//  function userLoggedMessage (username){
+//   return `${username} Welcome to the facebook Thanks for Joining us`
+//  }
+//  console.log(userLoggedMessage("Shreyash")); // Shreyash Welcome to the facebook Thanks for Joining us
+ 
+
+ function userLoggedMessage (username){
+
+    if(username === undefined){ // if username  is equal to undefined then print a message saying 
+        // please enter your username
+        console.log("Please enter your Username");
+        return // further code wont be executed ; here the part of string interpolation
+        
+    }
+
+    return `${username} Thanks for joing facbeook , you just logged in ` 
 
 }
- const Output = sumOfTwoNumbers(4, 5) 
- console.log( " Results:", Output);  //  Results: 9
- 
+console.log(userLoggedMessage()); // returns undefined if arguement is empty if it was ("Shreyash")
+// It would be Shreyash Thanks for Joing Facebook.
 
- 
+
+// also we could write if stuff in this way too if (!username){
+    //   console.log("Please enter your Username");
+    // }
+    // if username is not username then print "Please enter your username this 
+    // code is equivalent to writing 
+    // if(username===undefined){console.log("Please enter your username ")}
