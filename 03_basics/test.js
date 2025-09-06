3d56860ab5008d02e6e2b61f0315076b2f4c4af3
Shreyash <himeshreyash@gmail.com>
2025-09-06 09:35:21 +0000

    TrainingGround

--- 03_basics/test.js
+++ 03_basics/test.js
@@ -1,9 +1,43 @@
-function myFunction (Number1  , Number2){
- console.log(Number1*Number2);
- 
-}
- const Output = myFunction(4, 5) // 20 
- console.log( " Results:", Output);  Results: undefined 
- 
+// // // // function messageafteruserlogged (Username){
+        
+// // // //     return `${Username} thanks for Joining us , Welcome to Facebook`
+// // // // }
+// // // // console.log(messageafteruserlogged("Shreyash"));
 
- 
+
+
+// // // function PurchaseCart (...itempurchased){
+// // //    return itempurchased
+// // // }
+// // // console.log(PurchaseCart(2000, 6000 , 9000 , 700));
+
+
+
+// // function sumOfTwoNumbers (Number1, Number2){
+// //     return Number1 + Number2
+// // }
+// // console.log(sumOfTwoNumbers(20 , 15));
+
+
+
+// function users (objects){
+//      return  `Username is ${objects.username}, and github is ${objects.github}`
+
+// }
+// console.log(users({
+//     username : "Shreyash", 
+//     github : "101shreyash", 
+
+// })
+// );
+
+
+ // lets pass the  3rd value of Array in a function 
+
+const myarray = ["Shreyash", "Programming", "Maths", "Calasthenics", "GreatBooks", "Intellect"]
+function acessingarray (getarray){
+    return getarray[3]
+
+}
+
+console.log(acessingarray(myarray)); // Calasthenics
