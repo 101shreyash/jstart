02b3d6e9e47e698082d0d2c0230afcfbafa5eedb
Shreyash <himeshreyash@gmail.com>
2025-09-08 09:23:03 +0000

    userloggedmsg

--- 03_basics/test.js
+++ 03_basics/test.js
@@ -1,43 +1,19 @@
-// // // // function messageafteruserlogged (Username){
-        
-// // // //     return `${Username} thanks for Joining us , Welcome to Facebook`
-// // // // }
-// // // // console.log(messageafteruserlogged("Shreyash"));
-
-
-
-// // // function PurchaseCart (...itempurchased){
-// // //    return itempurchased
-// // // }
-// // // console.log(PurchaseCart(2000, 6000 , 9000 , 700));
-
-
-
-// // function sumOfTwoNumbers (Number1, Number2){
-// //     return Number1 + Number2
-// // }
-// // console.log(sumOfTwoNumbers(20 , 15));
-
-
-
-// function users (objects){
-//      return  `Username is ${objects.username}, and github is ${objects.github}`
-
-// }
-// console.log(users({
-//     username : "Shreyash", 
-//     github : "101shreyash", 
-
-// })
-// );
-
-
- // lets pass the  3rd value of Array in a function 
-
-const myarray = ["Shreyash", "Programming", "Maths", "Calasthenics", "GreatBooks", "Intellect"]
-function acessingarray (getarray){
-    return getarray[3]
+ 
+ const date = new Date()
+ date.toString()
+ 
+ 
+ const UserINfo = {
+    Name: "Kushal",
+    Action : "LoggedIn",
+    Location : "Tokyo, Japan",
+    Date : [date]
 
 }
 
-console.log(acessingarray(myarray)); // Calasthenics
+
+function userLoggedMessage (AcessingJson){
+ return `Dear USer You Account has been ${AcessingJson.Action} in Another device From ${AcessingJson.Location} at ${AcessingJson.Date}`
+
+}
+console.log(userLoggedMessage(UserINfo));
