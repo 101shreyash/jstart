451f58c8fe630843cb0d83b4d625fd1167025159
Shreyash <himeshreyash@gmail.com>
2025-09-27 14:05:48 +0000

    controlflow in js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,2 +1,36 @@
-// // Here we would be talking about Control Flow if statements , comaprision operator and many more
-// like else keyword
+// // if statement
+
+
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+
+/*
+
+<   // less than 
+
+>  // greater than 
+
+<= // less than equal to  
+
+ >=  // greater than equal to 
+
+==, // loose equality (compares values but ignores type)  5 == "5" → true (can be tricky)
+
+!=  // is not equal to 
+
+ === // strict equality (compares value and type)
+ 5 === "5" → false
+ 5 === 5 → true
+
+
+
+
+ */
+
+
