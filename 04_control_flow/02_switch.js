e8c9d92599ecee0a7ffc6df4fce0d5395a9a7579
Shreyash <himeshreyash@gmail.com>
2025-09-29 15:18:24 +0000

    ControlFlow&Switch in js

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3 // march
+
+switch (month) {
+    case 1: 
+    console.log("Month is January");
+    
+        break;  
+    case 2: 
+    console.log("Month is February");
+
+        break;
+    case 3: 
+    console.log("Month is March");
+
+        break;
+
+    default:
+        console.log("Default Case Match");
+            break;
+}
