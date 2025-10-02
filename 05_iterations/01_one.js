d0db2f9f1380776233e793e251a516ede393a8dd
Shreyash <himeshreyash@gmail.com>
2025-10-02 15:23:20 +0000

    forloop

---
+++ 05_iterations/01_one.js
@@ -0,0 +1,15 @@
+// FOR LOOP
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 7) {
+    console.log("That's the Jersey Number of Ronaldo");      
+    }
+    console.log(element);
+    
+}
+
+
+
+// prints the value from 0 to 10 
+
