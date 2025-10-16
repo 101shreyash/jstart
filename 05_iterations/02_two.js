25f9de9c5c88d78bba7391136071d6fdbcbbe7e1
Shreyash <himeshreyash@gmail.com>
2025-10-16 12:21:45 +0000

    While Loop & Do While Loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,38 @@
+
+
+ //   ---------------------------  WHILE LOOP ------------------------
+
+
+let index = 0
+
+while (index <= 10) {
+
+    // console.log(index);
+
+    index = index+2
+    
+}
+// Loops on Array using While LOOP  
+
+let hobbies = ["chess", "Calasthenics", "Programming", "Reading Piles of Great Books"]
+let randomindex = 0
+
+while (randomindex < hobbies.length) {
+
+    // console.log(` Shreyash Hobbies are ${hobbies[randomindex]}`);
+    
+
+    randomindex ++
+    
+}
+
+// ----------------------------- DO WHILE LOOP ------------------------------
+
+let score = 0
+
+do {
+
+    console.log(score);
+    score ++   
+    
+} while (score <= 10);
