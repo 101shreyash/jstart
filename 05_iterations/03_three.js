4d5b6c1ec46f886efeb47357e460ed42b85543b1
Shreyash <himeshreyash@gmail.com>
2025-10-17 09:51:03 +0000

    loops discussion

---
+++ 05_iterations/03_three.js
@@ -0,0 +1,29 @@
+// FOR OF LOOP
+
+
+// ["", "", ""]
+// [{},{},{}]
+
+
+const Hobbies = ["Chess", "Programming", "Calasthenics", "DistanceRunning", "ReadingGreatBooks", "Reading"]
+
+for (const index of Hobbies) {
+
+    // console.log(index);
+    
+    
+} // thats it simple asf less complex than any other loops
+//  Here we are using a "for...of" loop to iterate through an array.
+
+
+
+// not just in an array we can use for of loop even in strings 
+
+const greetings = "Hello World!"
+
+for (const greet of greetings) {
+    
+
+    console.log(element);
+    
+}
