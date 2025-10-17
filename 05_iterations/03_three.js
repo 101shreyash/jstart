f1bedce9d4be8049f14b1326678de38938ac507a
Shreyash <himeshreyash@gmail.com>
2025-10-17 13:28:50 +0000

    Maps , FOR IN & FOR OF

--- 05_iterations/03_three.js
+++ 05_iterations/03_three.js
@@ -19,11 +19,86 @@ for (const index of Hobbies) {
 
 // not just in an array we can use for of loop even in strings 
 
-const greetings = "Hello World!"
+// const greetings = "Hello World!"
 
-for (const greet of greetings) {
+// for (const greet of greetings) {
     
 
-    console.log(element);
-    
+//     console.log(greet);
+
+// }
+
+
+
+// ****************** Maps In JS *************
+
+
+// A Map is a special object in JS used to store key value pairs 
+//  kinda like an object, but with superpowers.
+// what diffrentiates it from object is  it Maintains order of insertions
+// is easy for iteration than obj 
+//  We can’t have duplicate keys each key must be unique.
+
+
+// const map = new Map ()
+// map.set ("Key" , "value")
+// map.set ("BiratnagarZipCode" , 56613)
+// map.set ("kathManduZipCode", 44600
+// )
+// console.log(map);
+
+
+// ******************************************
+
+
+ /* output :  Map(3) {
+  'Key' => 'value',
+  'BiratnagarZipCode' => 56613,
+  'kathManduZipCode' => 44600
 }
+  */
+
+
+
+// *********************** Iterations on Map ***********************
+
+
+const map = new Map ()
+map.set ("Key" , "value")
+map.set ("BiratnagarZipCode" , 56613)
+map.set ("kathManduZipCode", 44600
+)
+// console.log(map);
+
+
+for (const [key, value] of map) {
+
+    // console.log(`${key} - ${value}`);
+    
+} 
+// as simple as that the "key value" here is an identifier we can set it acc to out prefrenace like i even write element 1 and element 2 in bracket obviously [element1, element2] of map
+
+
+
+// ************************* Can We Use For Of Loop In Obj ?? *******************
+
+
+const userinfo = {
+
+    username : 'Shreyaash', 
+    surname : 'gautam',
+    location : "biratnagar"
+
+}
+
+for (const [key, value] of userinfo) {
+
+console.log(key,value);
+ 
+    
+} 
+// No we cant the output is "userinfo is not iterable"
+// Because plain objects in JS are not iterable by default.
+// for...of only works on iterable data structures like: Arrays , Strings , Maps , Sets
+
+
---
+++ 05_iterations/04_four.js
@@ -0,0 +1,39 @@
+const userinfo = {
+    username : 'Shreyash', 
+    favBook : "Zero to One By Peter theil ",
+    favMovie : "Rocky IV of Sylvester Stallone and DulphLundergren",
+    favSwimmer : "Miacheal Philips",
+    favFiction : "AnimalFarm By George Orwell",
+}
+
+for (const key in userinfo) {
+
+// console.log(`${key} - ${userinfo[key]}`);
+    
+    
+    
+} // as simple as that 
+
+
+
+// **************** Now the Question Can We Use For In Loop For Arraays *************
+
+const favBooktillNow = ["ZeroToOne" ,"AtomicHabit", "SameAsEver", "YouSquare"]
+
+for (const key in favBooktillNow) {
+
+console.log(favBooktillNow[key]);
+
+          
+} 
+
+// yes we can
+// The Catch (Why pros don’t use for...in for arrays)
+
+// for...in is made for objects, not arrays.
+// It loops through keys (indexes) — as strings, not numbers.
+
+// And if you or some library adds extra properties to the array (like .custom = "something"),
+// for...in will loop through those too, which can mess up your logic.
+
+// so the better alternative would to use forof in arrays
