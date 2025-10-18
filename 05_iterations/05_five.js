669e6fccde9cdaa6bc737cf2cf0b0aa52d4e4604
Shreyash <himeshreyash@gmail.com>
2025-10-18 14:31:46 +0000

    High Order Loops

---
+++ 05_iterations/05_five.js
@@ -0,0 +1,10 @@
+// FOR EACH LOOP
+
+
+const hobbies = ["Chess", "DistanceRunning" , "Programming", "Calasthenics", "Reading"]
+
+
+hobbies.forEach(function (item) {
+    console.log(item);
+    
+} ) 
