516034bc8a1ffe3d265cfedb568ad31b2f2aea71
Shreyash <himeshreyash@gmail.com>
2025-10-19 14:45:53 +0000

    Higher Order Array LOOPS

--- 05_iterations/05_five.js
+++ 05_iterations/05_five.js
@@ -1,10 +1,29 @@
-// FOR EACH LOOP
+// FOR EACH LOOP (IS HIGHER ORDER ARRAY LOOP )
+
+// for...of → simple, clean, works with break/continue. Fast for loops where you might need control.
+// forEach → functional, modern, can’t use break or continue. Good for clean one-liners or callbacks.
+// If you need flexibility, go for...of.
+// If you want readable functional style, go forEach.
+
+// you can call forEach a higher-order loop
+//  because it loops over an array and takes a function as input.
+
+
+
+// ***************** USING Function Declaration *****************
 
 
 const hobbies = ["Chess", "DistanceRunning" , "Programming", "Calasthenics", "Reading"]
 
 
 hobbies.forEach(function (item) {
-    console.log(item);
+    // console.log(item);
     
 } ) 
+
+// ************************* USING ARROW FUNCTION ************************
+
+const thingsidoinmeantime = ["mathsOviously", "VideosOfSamOvens", "podcastofLexFriedman", "VideosByYC"]
+
+thingsidoinmeantime.forEach ((thisisparameter) => {console.log(thisisparameter);
+});
---
+++ 05_iterations/06_test.js
@@ -0,0 +1,7 @@
+const thingsidoinmeantime = ["mathsOviously", "VideosOfSamOvens", "podcastofLexFriedman", "VideosByYC"]
+
+thingsidoinmeantime.forEach ((thisisparameter) => {console.log(thisisparameter);
+});
+
+
+// here this is the parameter coud be anything from item to thing there's no issue to it
