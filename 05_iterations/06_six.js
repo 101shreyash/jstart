9f1012f60157451cc286875c34499d8ac200bc13
Shreyash <himeshreyash@gmail.com>
2025-10-24 12:13:27 +0000

    FilterMap & Reduce In Js

--- 05_iterations/06_six.js
+++ 05_iterations/06_six.js
@@ -87,6 +87,10 @@ console.log(userBooks);
 // now the user asked us to give all the books that were published after 2000
 
 
+
+
+
+
  userBooks = books.filter ( (publishedDate) => {return publishedDate.publish >= 2000 })
  // here we are reassigning to the same varible named userBooks Cause FrontEnd Mofos would use this same 
  // varible called userBooks
@@ -98,4 +102,23 @@ console.log(userBooks);
 
 
 
- 
+//  ************************** AND OPERATOR USE CASE IN REDUCE **********************
+
+ const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+
+ const HistoryBookies = books.filter ( (HistoryBooks) => {return HistoryBooks.genre === "History"  && HistoryBooks.publish < 1990 })
+
+console.log(HistoryBookies); // we could use &&  Inorder to get multiple Values 
+// lets says we were making a webpage for car so we could filter multiple Parameters such as 
+// car colour &&  cartype && CarModel && Carbrand using " AND OPERATOR "
