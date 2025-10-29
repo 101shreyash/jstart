47eda7d507e59e927f362c490b2ae054ad73d8b1
Shreyash <himeshreyash@gmail.com>
2025-10-29 15:26:00 +0000

    Map And Redduce In Js

---
+++ 05_iterations/07_seven.js
@@ -0,0 +1,20 @@
+// adding + 10 on each and ever number of an array using both foor loop And Map
+
+
+//  ********************** Using Maps *****************
+
+
+const allthenums = [1,2,3,4,5,6,7,8,9,10]
+
+// const afteradding = allthenums.map ( (adding10toEachofThem) => {return adding10toEachofThem + 10 } )
+// console.log(afteradding);
+
+
+// ******************** Using For Each ************************
+
+
+const  somenums = [1,2,3,4,5,6,7,8,9,10]
+
+somenums.forEach ( (afteradding) => {console.log(afteradding + 10);})
+
+
